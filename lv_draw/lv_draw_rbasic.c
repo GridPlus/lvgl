5670bea3caac4dc53f728f2eb26c1c56d98c04d9
stu westerman <stuart.westerman@gridplus.io>
2020-10-06 11:47:03 -0500

    Code cleanup and trying to understand why the 90 degree rotation and transposition.

--- lv_draw/lv_draw_rbasic.c
+++ lv_draw/lv_draw_rbasic.c
@@ -284,15 +284,13 @@ void lv_rmap(const lv_area_t * cords_p, const lv_area_t * mask_p,
     /*If there are common part of the mask and map then draw the map*/
     if(union_ok == false) return;
 
-    lv_color_t img_buffer[masked_a.x2 - masked_a.x1 + 1];
-
     /*Go to the first pixel*/
     lv_coord_t map_width = lv_area_get_width(cords_p);
     lv_coord_t mask_w = lv_area_get_width(&masked_a) - 1;
     map_p += (masked_a.y1 - cords_p->y1) * map_width * sizeof(lv_color_t);
     map_p += (masked_a.x1 - cords_p->x1) * sizeof(lv_color_t);
 
-    volatile lv_coord_t row = 0;
+    lv_coord_t row;
     if(recolor_opa == LV_OPA_TRANSP && chroma_key == false) {
         for(row = masked_a.y1; row <= masked_a.y2; row++) {
             lv_disp_map(masked_a.x1, row, masked_a.x1 + mask_w, row, (lv_color_t *)map_p);
@@ -300,7 +298,7 @@ void lv_rmap(const lv_area_t * cords_p, const lv_area_t * mask_p,
         }
     } else {
         lv_color_t chroma_key_color = LV_COLOR_TRANSP;
-        lv_coord_t col = 0;
+        lv_coord_t col;
         for(row = masked_a.y1; row <= masked_a.y2; row++) {
             for(col = masked_a.x1; col <= masked_a.x2; col++) {
                 lv_color_t * px_color = (lv_color_t *) &map_p[(uint32_t)(col - masked_a.x1) * sizeof(lv_color_t)];
@@ -312,7 +310,6 @@ void lv_rmap(const lv_area_t * cords_p, const lv_area_t * mask_p,
 
                     lv_rpx(col, row, mask_p, recolored_px, LV_OPA_COVER);
                 } else {
-                    img_buffer[col - masked_a.x1] = *px_color;
                     //lv_rpx(col, row, mask_p, *px_color, LV_OPA_COVER);
                 }
             }
